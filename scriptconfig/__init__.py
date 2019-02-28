96885f1742384fbabeab8176e2b6e125ffa33151
joncrall <erotemic@gmail.com>
2019-02-27 20:30:49 -0500

    wip

--- scriptconfig/__init__.py
+++ scriptconfig/__init__.py
@@ -4,10 +4,12 @@ mkinit ~/code/scriptconfig/scriptconfig/__init__.py
 
 __version__ = '0.3.0.dev0'
 
-from .config import (Config,)
+from .config import (Config, DataInterchange, scfg_isinstance,)
 from .dict_like import (DictLike,)
 from .file_like import (FileLike,)
+from .smartcast import (BooleanType, NoneType, smartcast,)
 from .value import (Path, PathList, Value,)
 
-__all__ = ['Config', 'DictLike', 'FileLike',
-           'Path', 'PathList', 'Value']
+__all__ = ['BooleanType', 'Config', 'DataInterchange', 'DictLike', 'FileLike',
+           'NoneType', 'Path', 'PathList', 'Value', 'scfg_isinstance',
+           'smartcast']
