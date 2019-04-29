5efd7249bc5a0bf414f05e30c0f8e0bd4c5d878c
joncrall <jon.crall@kitware.com>
2019-04-29 13:18:00 -0400

    Fix drone issue

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -3,6 +3,6 @@
 # Install dependency packages
 pip install -r requirements.txt
 
-# Install irharn in developer mode
+# Install in developer mode
 python setup.py develop
-#pip install -e .
+#pip install -e .  # cant do this anymore
