2b7b7693bdd5d8da7e103b810c698990052353da
Jon Crall <jon.crall@kitware.com>
2019-09-17 13:03:45 -0400

    wip

--- scriptconfig/config.py
+++ scriptconfig/config.py
@@ -75,7 +75,7 @@ TODO
     - [X] Rename
     - [ ] Handle Nested Configs?
     - [X] Write docs?
-    - [ ] Public Release?
+    - [X] Public Release?
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import ubelt as ub
