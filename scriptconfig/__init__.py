efbab35c43481b663a99f3722c600b9cbc29d2a1
joncrall <erotemic@gmail.com>
2019-11-28 15:09:58 -0500

    Add express cli

--- scriptconfig/__init__.py
+++ scriptconfig/__init__.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
 """
 mkinit ~/code/scriptconfig/scriptconfig/__init__.py --nomods --relative
 """
 
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 from .config import (Config,)
 from .value import (Path, PathList, Value,)
+from .cli import (quick_cli,)
 
-__all__ = ['Config', 'Path', 'PathList', 'Value']
+__all__ = ['Config', 'Path', 'PathList', 'Value', 'quick_cli']
