3853d4f9a174b024292662f22a41536de541b930
joncrall <jon.crall@kitware.com>
2022-06-09 20:52:53 -0400

    final doc tweak

--- scriptconfig/__init__.py
+++ scriptconfig/__init__.py
@@ -23,6 +23,7 @@ Here is an example for a simple calculator program:
 
     import scriptconfig as scfg
 
+
     class MyConfig(scfg.Config):
         'The docstring becomes the CLI description!'
         default = {
@@ -31,14 +32,22 @@ Here is an example for a simple calculator program:
             'outfile': './result.txt',
         }
 
+
     def main():
         config = MyConfig(cmdline=True)
         result = config['num1'] + config['num2']
         with open(config['outfile'], 'w') as file:
             file.write(str(result))
 
+
     if __name__ == '__main__':
         main()
+
+If the above is written to a file `calc.py`, it can be be called like this.
+
+.. code:: bash
+
+    python calc.py --num1=3 --num2=4 --outfile=/dev/stdout
 
 It is possible to gain finer control over the CLI by specifying the values in
 ``default`` as a :class:`scriptconfig.Value`, where you can specify a help
@@ -57,6 +66,7 @@ A more complex example version of the above code might look like this
 
     import scriptconfig as scfg
 
+
     class MyConfig(scfg.Config):
         '''
         The docstring becomes the CLI description!
@@ -66,6 +76,7 @@ A more complex example version of the above code might look like this
             'num2': scfg.Value(1, type=float, help='second number to add', position=2),
             'outfile': scfg.Value('./result.txt', help='where to store the result', position=3),
         }
+
 
     def main(cmdline=1, **kwargs):
         '''
@@ -82,8 +93,44 @@ A more complex example version of the above code might look like this
         with open(config['outfile'], 'w') as file:
             file.write(str(result))
 
+
     if __name__ == '__main__':
         main()
+
+This code can be called with positional arguments:
+
+.. code:: bash
+
+    python calc.py 33 44 /dev/stdout
+
+
+The help text for this program (via ``python calc.py --help``) looks like this:
+
+.. code::
+
+    usage: MyConfig [-h] [--num1 NUM1] [--num2 NUM2] [--outfile OUTFILE] [--config CONFIG] [--dump DUMP] [--dumps] num1 num2 outfile
+
+    The docstring becomes the CLI description!
+
+    positional arguments:
+      num1               first number to add
+      num2               second number to add
+      outfile            where to store the result
+
+    optional arguments:
+      -h, --help         show this help message and exit
+      --num1 NUM1        first number to add (default: 0)
+      --num2 NUM2        second number to add (default: 1)
+      --outfile OUTFILE  where to store the result (default: ./result.txt)
+      --config CONFIG    special scriptconfig option that accepts the path to a on-disk configuration file, and loads that into this 'MyConfig' object. (default: None)
+      --dump DUMP        If specified, dump this config to disk. (default: None)
+      --dumps            If specified, dump this config stdout (default: False)
+
+
+Note that keyword arguments are always available, even if the argument is
+marked as positional. This is because a scriptconfig object always reduces to
+key/value pairs --- i.e. a dictionary.
+
 
 See the :mod:`scriptconfig.config` module docs for details and examples on
 getting started.
