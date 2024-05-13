6067fb6dc8772989b7623e6fda87da3a74780db2
joncrall <jon.crall@kitware.com>
2024-05-13 12:59:33 -0400

    Start branch for 0.7.16

--- scriptconfig/__init__.py
+++ scriptconfig/__init__.py
@@ -151,7 +151,7 @@ Ignore:
     mkinit ~/code/scriptconfig/scriptconfig/__init__.py --nomods --relative -w
 """
 
-__version__ = '0.7.15'
+__version__ = '0.7.16'
 
 __submodules__ = {
     'modal': None,
