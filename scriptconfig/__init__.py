a4e46faadc4a64c1738cf6eea6f4cf1ba5ddadcc
joncrall <jon.crall@kitware.com>
2025-10-10 19:23:09 -0400

    [skip ci] Start branch for 0.8.4

--- scriptconfig/__init__.py
+++ scriptconfig/__init__.py
@@ -145,7 +145,7 @@ Ignore:
     mkinit ~/code/scriptconfig/scriptconfig/__init__.py --nomods --relative -w
 """
 
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 
 __submodules__ = {
     'modal': None,
